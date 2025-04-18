35e1fb50a9683e275152b32bb5a5a13352b4607e
plojo <plonjo3@gmail.com>
2025-04-18 17:10:16 +0200

    add toggle for floor

--- Script/Build/Script.js
+++ Script/Build/Script.js
@@ -1,1 +1,70 @@
-console.info("%cTo start scripting, run the typescript compiler in PROJECTFOLDER/Script/Source/. This is also where your code should be. Use any IDE you like.", "color: green; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static { this.iSubclass = ƒ.Component.registerSubclass(CustomComponentScript); }
+        constructor() {
+            super();
+            // Properties may be mutated by users in the editor via the automatically created user interface
+            this.message = "CustomComponentScript added to ";
+            // Activate the functions of this component as response to events
+            this.hndEvent = (_event) => {
+                switch (_event.type) {
+                    case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
+                        ƒ.Debug.log(this.message, this.node);
+                        break;
+                    case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
+                        this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+                        this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+                        break;
+                    case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
+                        // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                        break;
+                }
+            };
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
+        }
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var f = FudgeCore;
+    f.Debug.info("Main Program Template running!");
+    let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
+    let graph;
+    function start(_event) {
+        viewport = _event.detail;
+        viewport.camera.node.addComponent(new f.ComponentAmbientOcclusion());
+        graph = viewport.getBranch();
+        let toggled = false;
+        let nodeFloor = graph.getChildrenByName("Floor")[0];
+        nodeFloor.activate(!toggled); // hide the floor node
+        let nodeFloorPlan = graph.getChildrenByName("FloorPlan")[0];
+        nodeFloorPlan.activate(toggled); // show the floorplan node
+        let btnToggleFloorPlan = document.getElementById("toggleFloorPlan");
+        btnToggleFloorPlan.onclick = () => {
+            toggled = !toggled;
+            nodeFloor.activate(!toggled); // hide the floor node
+            nodeFloorPlan.activate(toggled); // show the floorplan node
+        };
+        f.Loop.addEventListener("loopFrame" /* f.EVENT.LOOP_FRAME */, update);
+        f.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        // ƒ.Physics.simulate();  // if physics is included and used
+        viewport.draw();
+        f.AudioManager.default.update();
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- Script/Source/Main.ts
+++ Script/Source/Main.ts
@@ -1,20 +1,35 @@
 namespace Script {
-  import ƒ = FudgeCore;
-  ƒ.Debug.info("Main Program Template running!");
+  import f = FudgeCore;
+  f.Debug.info("Main Program Template running!");
 
-  let viewport: ƒ.Viewport;
+  let viewport: f.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+  let graph: f.Node;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
+    viewport.camera.node.addComponent(new f.ComponentAmbientOcclusion());
+    graph = viewport.getBranch();
 
-    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    let toggled: boolean = false;
+    let nodeFloor: f.Node = graph.getChildrenByName("Floor")[0];
+    nodeFloor.activate(!toggled); // hide the floor node
+    let nodeFloorPlan: f.Node = graph.getChildrenByName("FloorPlan")[0];
+    nodeFloorPlan.activate(toggled); // show the floorplan node
+    let btnToggleFloorPlan: HTMLButtonElement = document.getElementById("toggleFloorPlan") as HTMLButtonElement;
+    btnToggleFloorPlan.onclick = () => {
+      toggled = !toggled;
+      nodeFloor.activate(!toggled); // hide the floor node
+      nodeFloorPlan.activate(toggled); // show the floorplan node
+    }
+
+    f.Loop.addEventListener(f.EVENT.LOOP_FRAME, update);
+    f.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
-    ƒ.AudioManager.default.update();
+    f.AudioManager.default.update();
   }
 }
