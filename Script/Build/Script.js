1b2384d3897a5bc6bf22161be1cb9e1b733dc8e0
plojo <plonjo3@gmail.com>
2025-04-18 16:55:24 +0200

    init

---
+++ Script/Build/Script.js
@@ -0,0 +1,1 @@
+console.info("%cTo start scripting, run the typescript compiler in PROJECTFOLDER/Script/Source/. This is also where your code should be. Use any IDE you like.", "color: green; font-weight: bold;");
